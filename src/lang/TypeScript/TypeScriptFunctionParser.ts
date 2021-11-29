d3a12b3fb4fdbad2f8d34df1daaa36450282101f
JasperGeer <jasper.geer@gmail.com>
2021-11-29 13:30:48 -0500

    Update TypeScriptFunctionParser.ts

--- src/lang/TypeScript/TypeScriptFunctionParser.ts
+++ src/lang/TypeScript/TypeScriptFunctionParser.ts
@@ -3,34 +3,37 @@ import { ParseTree } from "antlr4ts/tree/ParseTree";
 import { TypeScriptLexer } from "../../antlr/TypeScript/TypeScriptLexer";
 import { TypeScriptParser } from "../../antlr/TypeScript/TypeScriptParser";
 import { DMErrorStrategy } from "../DMErrorStrategy";
+import { FunctionData } from "../FunctionData";
 import { FunctionParser } from "../FunctionParser";
 import { TypeScriptFunctionVisitor } from "./TypeScriptFunctionVisitor";
 
 export class TypeScriptFunctionParser implements FunctionParser {
     private tree: ParseTree | null = null;
     private visitor: TypeScriptFunctionVisitor;
-    private paramNames: string[] = [];
-    private returnType: string = "";
-    private exceptions: string[] = [];
+    private funcData: FunctionData;
 
     constructor(chars: CharStream) {
+        this.funcData = {
+            paramNames: [],
+            returnType: "",
+            exceptions: []
+        };
         this.visitor = new TypeScriptFunctionVisitor();
         let lexer: Lexer = new TypeScriptLexer(chars);
         let tokens: TokenStream = new CommonTokenStream(lexer);
         let parser = new TypeScriptParser(tokens);
-        let errorStrategy = new DMErrorStrategy();
         this.tree = parser.program();
-        this.visitor.visit(this.tree);
+        this.funcData = this.visitor.visit(this.tree);
     }
 
     getParamNames(): string[] {
-        throw new Error("Method not implemented.");
+        return this.funcData.paramNames;
     }
     getReturnType(): string {
-        throw new Error("Method not implemented.");
+        return this.funcData.returnType;
     }
     getExceptions(): string[] {
-        throw new Error("Method not implemented.");
+        return this.funcData.exceptions;
     }
     
 }
