3b18f8d4d7c7df386885358892a80a6c1b234f53
JasperGeer <jasper.geer@gmail.com>
2021-11-29 13:30:28 -0500

    Update TypeScriptFunctionVisitor.ts

--- src/lang/TypeScript/TypeScriptFunctionVisitor.ts
+++ src/lang/TypeScript/TypeScriptFunctionVisitor.ts
@@ -1,5 +1,6 @@
 import { AbstractParseTreeVisitor } from "antlr4ts/tree/AbstractParseTreeVisitor";
-import { FunctionDeclarationContext, ProgramContext, SourceElementsContext } from "../../antlr/TypeScript/TypeScriptParser";
+import { ParseTree } from "antlr4ts/tree/ParseTree";
+import { CallSignatureContext, FunctionDeclarationContext, ParameterListContext, ProgramContext } from "../../antlr/TypeScript/TypeScriptParser";
 import { TypeScriptParserVisitor } from "../../antlr/TypeScript/TypeScriptParserVisitor";
 import { FunctionData } from "../FunctionData";
 
@@ -18,12 +19,50 @@ export class TypeScriptFunctionVisitor extends AbstractParseTreeVisitor<Function
 
     visitProgram(ctx: ProgramContext): FunctionData {
         if (ctx.sourceElements()?.sourceElement()[0].statement().functionDeclaration()) {
-            return this.visitFunctionDeclaration(ctx.sourceElements()!.sourceElement()[0].statement().functionDeclaration()!);
+            return this.visitCallSignature(ctx.sourceElements()!.sourceElement()[0].statement().functionDeclaration()!.callSignature());
         }
         return this.defaultResult();
     }
 
-    visitFunctionDeclaration(ctx: FunctionDeclarationContext): FunctionData {
+    visitCallSignature(ctx: CallSignatureContext):FunctionData {
+        let result: FunctionData = {
+            paramNames: [],
+            returnType: "",
+            exceptions: []
+        };
+        if (ctx.parameterList()) {
+            result.paramNames = this.visitParameterList(ctx.parameterList()!).paramNames;
+        }
+        if (ctx.typeAnnotation()) {
+            result.returnType = ctx.typeAnnotation()!.type_().text;
+        }
+        return result;
+    }
 
+    visitParameterList(ctx: ParameterListContext): FunctionData {
+        let result: FunctionData = {
+            paramNames: [],
+            returnType: "",
+            exceptions: []
+        };
+        for (let param of ctx.parameter()) {
+            if (param.requiredParameter()) {
+                console.log(param.requiredParameter()?.identifierOrPattern().identifierName()?.text);
+                result.paramNames.push(this.getLeftmostLeaf(param.requiredParameter()!).text);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get the leftmost leaf of a given parse tree
+     * @param ctx parse tree
+     * @returns leftmost leaf of parse tree
+     */
+    getLeftmostLeaf(ctx: ParseTree): ParseTree {
+        if (ctx.childCount > 0) {
+            return this.getLeftmostLeaf(ctx.getChild(0));
+        }
+        return ctx;
     }
 }
