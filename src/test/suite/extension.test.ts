3ba7d7cbd1d205f850b1196a0cebfe9a0ecb4969
JasperGeer <jasper.geer@gmail.com>
2021-11-30 13:39:06 -0500

    Update extension.test.ts

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -36,4 +36,73 @@ suite('C Test Suite', () => {
         assert(parser.getParamNames()[1] === "b");
 	});
     
+    test('Array single parameter test', () => {
+        let input: string = "int func(int i[]);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 1);
+        assert(parser.getParamNames()[0] === "i");
+	});
+
+    test('Array multiple parameter test', () => {
+        let input: string = "int func(int i[][], char j[]);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 2);
+        assert(parser.getParamNames()[0] === "i");
+        assert(parser.getParamNames()[1] === "j");
+	});
+
+    test('Pointer single parameter test', () => {
+        let input: string = "int func(int *i);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 1);
+        assert(parser.getParamNames()[0] === "i");
+	});
+
+    test('Pointer multiple parameter test', () => {
+        let input: string = "int func(int *i, char **j, long *k);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 3);
+        assert(parser.getParamNames()[0] === "i");
+        assert(parser.getParamNames()[1] === "j");
+        assert(parser.getParamNames()[2] === "k");
+	});
+
+    test('Pointer return type test', () => {
+        let input: string = "int *func(int *i, char **j, long *k);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int*");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 3);
+        assert(parser.getParamNames()[0] === "i");
+        assert(parser.getParamNames()[1] === "j");
+        assert(parser.getParamNames()[2] === "k");
+	});
+
+    test('Double pointer return type test', () => {
+        let input: string = "int **func(int *i, char **j, long *k);";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "int**");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 3);
+        assert(parser.getParamNames()[0] === "i");
+        assert(parser.getParamNames()[1] === "j");
+        assert(parser.getParamNames()[2] === "k");
+	});
+
+    test('Function pointer single parameter test', () => {
+        let input: string = "void func ( void (*f)(int) );";
+        let parser: CFunctionParser = new CFunctionParser(CharStreams.fromString(input));
+        assert(parser.getReturnType() === "");
+        assert(parser.getExceptions().length === 0);
+        assert(parser.getParamNames().length === 1);
+        assert(parser.getParamNames()[0] === "f");
+	});
 });
